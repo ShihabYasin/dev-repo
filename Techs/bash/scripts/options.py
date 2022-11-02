764c946b5f8b3c1a46b31323bc80634f5f3ac207
Yasin-User-0001-at-858365001017 <Yasin-User-0001-at-858365001017>
2022-11-02 16:49:57 +0600

    Repo Updated

--- Techs/bash/scripts/options.py
+++ Techs/bash/scripts/options.py
@@ -1,9 +1,12 @@
 import os
 
+script_path = "~/StudyProjects/dev-repo"
+
 print(f"""
       0: shut down
       1: reboot
       2: monitor reset
+      3: git commit dev-repo
       
       """)
 
@@ -12,6 +15,11 @@ if var == "0":
     os.system("poweroff")
 elif var == "1":
     os.system("reboot")
+elif var == "2":
+    os.system("rm -rf /etc/X11/xorg.conf")
+    os.system("poweroff")
+elif var == "3":
+    os.system(f".{script_path}/git_commit_auto_script.sh")
 else:
     print("Bye Bye !!!")
     
